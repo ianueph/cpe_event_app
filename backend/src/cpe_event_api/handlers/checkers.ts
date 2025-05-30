4a7ffe47821a03f9b95be262fba15fa2d168d341
ianueph <111487555+ianueph@users.noreply.github.com>
2025-05-31 01:44:15 +0800

    Create handlers folder

---
+++ backend/src/cpe_event_api/handlers/checkers.ts
@@ -0,0 +1,31 @@
+import db from "../db"
+
+export async function eventIdExists(id : number) : Promise<boolean> {
+    const result = await db.query({
+        name: 'check-event-exists',
+        text: 'SELECT 1 FROM events WHERE id = $1',
+        values: [id]
+    })
+
+    return result.rows.length > 0;      
+}
+
+export async function studentNumberExists(student_number : string) : Promise<boolean> {
+    const result = await db.query({
+        name: 'check-student-exists',
+        text: 'SELECT 1 FROM students WHERE student_number = $1',
+        values: [student_number]
+    })
+
+    return result.rows.length > 0;      
+}
+
+export async function attendeeIdExists(id : number) : Promise<boolean> {
+    const result = await db.query({
+        name: 'check-attendee-exists',
+        text: 'SELECT 1 FROM attendees WHERE id = $1',
+        values: [id]
+    })
+
+    return result.rows.length > 0;     
+}
