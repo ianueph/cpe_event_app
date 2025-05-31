6a5d592fb0f65c0b22fd2d2124f004682d539139
ianueph <111487555+ianueph@users.noreply.github.com>
2025-05-31 18:45:17 +0800

    Handle ZodErrors separately in errorHandler

--- backend/src/middlewares.ts
+++ backend/src/middlewares.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 import ErrorResponse from './interfaces/ErrorResponse';
+import { ZodError } from 'zod/v4';
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
   res.status(404);
@@ -12,6 +13,15 @@ export function notFound(req: Request, res: Response, next: NextFunction) {
 export function errorHandler(err: Error, req: Request, res: Response<ErrorResponse>, next: NextFunction) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
+
+  if (err instanceof ZodError) {
+    res.json({
+      message: err.message,
+      issues: err.issues,
+    })
+    return;
+  }
+
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
