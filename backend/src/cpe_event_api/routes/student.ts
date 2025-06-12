236c69874ab8c77e98a93656d32c499e4735463e
ianueph <111487555+ianueph@users.noreply.github.com>
2025-06-12 18:19:57 +0800

    Initialize student routes

--- backend/src/cpe_event_api/routes/student.ts
+++ backend/src/cpe_event_api/routes/student.ts
@@ -1,7 +1,31 @@
 /*
-GET 	/students/:event_id
+GET 	/students/:student_number
 GET 	/students/
 POST	/students/
-UPDATE	/students/:event_id
+UPDATE	/students/:student_number
+DELETE	/students/:student_number 	TODO: Batch
 */
 
+import express from "express";
+
+require('express-async-errors');
+const router = express.Router()
+
+router.param('student_number', async (req, res, next) => {
+})
+
+router.route("/")
+	.get(async (req, res) => {
+	})
+	.post(async (req, res) => {
+	})
+
+router.route('/:student_number')
+	.get(async (req, res) => {
+	})
+	.put(async (req, res) => {
+	})
+	.delete(async (req, res) => {
+	})
+
+export default router;
